36ef5d171ef3097299b9a85dca6b07f57eb9576d
Gaurav <theregaurav08@gmail.com>
2025-06-10 18:39:32 +0530

    Hello Java

---
+++ helloWorld.java
@@ -0,0 +1,6 @@
+public class helloWorld {
+    //Begining of the journey without this is not right!!
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
